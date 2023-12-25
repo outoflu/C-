8b09d235e3b295c26927f120e807651ef6101af8
outoflu <jiangdongxian@outlook.com>
2023-12-25 10:42:47 +0800

    12.25

---
+++ cpprender/core/graphics.h
@@ -0,0 +1,71 @@
+#ifndef _GRAPHICS_H
+#define _GRAPHICS_H
+
+#include "maths.h"
+
+namespace core{
+
+class framebuffer_t{
+private:
+    int width, height;
+public:    
+    void set_width(int _width);
+    int get_width();
+
+    void set_height(int _height);
+    int get_height();
+
+    framebuffer_t(int _width,int _height);
+    ~framebuffer_t();
+    framebuffer_t(const framebuffer_t&)=delete;
+    framebuffer_t operator=(const framebuffer_t&)=delete;
+
+    void clear_color(vec4_t _color);
+    void clear_depth(float _clear);
+
+    unsigned char* color_buffer;
+    float* depth_buffer;
+};
+
+
+/* program management */
+
+#define MAX_VARYINGS 10
+
+template <typename _Attribs,typename _Varyings,typename _Uniforms>
+struct program_t {
+    using vertex_shader_t=vec4_t(*)(_Attribs,_Varyings,_Uniforms);
+    using fragment_shader_t=vec4_t(*)(_Varyings,_Uniforms,int&,int);
+    vertex_shader_t vertex_shader;
+    fragment_shader_t fragment_shader;
+
+    int double_sided;
+    int enable_blend;
+    /* for shaders */
+    _Attribs  shader_attribs[3];
+    _Varyings shader_varyings;
+    _Uniforms shader_uniforms;
+    /* for clipping */
+    vec4_t in_coords[MAX_VARYINGS];
+    vec4_t out_coords[MAX_VARYINGS];
+    _Varyings in_varyings[MAX_VARYINGS];
+    _Varyings out_varyings[MAX_VARYINGS];
+
+    program_t(vertex_shader_t,fragment_shader_t,int double_sided,int enable_blend);
+    _Attribs get_attribs(int nth_vertex);
+    _Uniforms get_uniforms();
+
+    program_t(const program_t&)=delete;
+    program_t operator=(const program_t&)=delete;
+    
+};
+
+
+/* graphics pipeline */
+template<typename _Attribs, typename _Varyings,typename _Uniforms>
+void graphics_draw_triangle(framebuffer_t& framebuffer, program_t<_Attribs,_Varyings,_Uniforms>& program);
+
+
+
+} // core 
+#endif //_GRAPHICS_H
---
+++ cpprender/core/image.h
@@ -0,0 +1,44 @@
+#ifndef _IMAGE_H
+#define _IMAGE_H
+
+#include <memory>
+
+namespace core{
+
+enum format_t {
+    FORMAT_LDR,
+    FORMAT_HDR
+};
+
+class image_t {
+public:
+    format_t format;
+    int width, height, channels;
+
+    unsigned char *ldr_buffer;
+    float *hdr_buffer;
+    image_t();
+    image_t(const image_t&)=delete;
+    image_t operator=(const image_t&)=delete;
+    image_t(int width,int height,int channels,format_t format);
+    void image_create(int width,int height,int channels,format_t format);
+    void image_load(const char* filename);
+    void image_save(const char* filename);
+    void image_flip_h();
+    void image_flip_v();
+    ~image_t();
+
+};
+
+/* image creating/releasing */
+//image_t *image_create(int width, int height, int channels, format_t format);
+//void image_release(image_t *image);
+//image_t *image_load(const char *filename);
+//void image_save(image_t *image, const char *filename);
+/* image processing */
+//void image_flip_h(image_t *image);
+//void image_flip_v(image_t *image);
+
+
+} //core
+#endif //_IMAGE_H
